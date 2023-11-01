f919a9b407044473ff487bd2ab626dad3bade87e
Daniel Churchill <danielbchurchill@gmail.com>
2023-11-01 15:15:04 -0400

    built app

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,11 @@
 // vite.config.js
-const { resolve } = require('path')
-const { defineConfig } = require('vite')
+const {
+    resolve
+}
+= require('path');
+const {
+    defineConfig
+}
+= require('vite');
 
-module.exports = defineConfig({
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-        single: resolve(__dirname, 'single_sketch.html'),
-        multi: resolve(__dirname, 'multi_sketch.html')
-      }
-    }
-  }
-})
+module.exports = defineConfig({ build: { rollupOptions: { input: { main: resolve(__dirname, 'index.html') } } } })
